f700e82b27d01852d06421c4c5ae7380c657af2f
Charles Grimes <charlesgrimes90@gmail.com>
2024-05-20 17:42:35 +0100

    Changed lambdas inputs to handle step function invocation

--- emailBuilder/src/main/scala/grimes/charles/Main.scala
+++ emailBuilder/src/main/scala/grimes/charles/Main.scala
@@ -4,11 +4,13 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import org.apache.logging.log4j.LogManager
 
-class Main extends RequestHandler[String, Unit] {
+import java.util
+
+class Main extends RequestHandler[util.HashMap[String, String], Unit] {
   private val logger = LogManager.getLogger(this.getClass)
 
-  def handleRequest(input: String, context: Context): Unit = {
-    logger.info(s"input received = $input")
+  def handleRequest(input: util.HashMap[String, String], context: Context): Unit = {
+    logger.info(s"input = $input")
     logger.info(s"name = ${context.getFunctionName}")
     logger.info(s"aws request id = ${context.getAwsRequestId}")
     logger.info(s"version = ${context.getFunctionVersion}")
