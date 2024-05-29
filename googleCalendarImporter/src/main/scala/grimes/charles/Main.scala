a6aceb7d3f5d696733f47803ed2154e4d1919814
Charles Grimes <charlesgrimes90@gmail.com>
2024-05-29 14:33:57 +0100

    Some more tidying in prep for unit testing

--- googleCalendarImporter/src/main/scala/grimes/charles/Main.scala
+++ googleCalendarImporter/src/main/scala/grimes/charles/Main.scala
@@ -6,18 +6,21 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import grimes.charles.calendar.CalendarService
 import grimes.charles.credentials.CredentialsLoader
-import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.{LogManager, Logger}
+
 import java.util
 import scala.jdk.CollectionConverters.*
 
 class Main extends RequestHandler[util.HashMap[String, String], String] {
-  private val logger = LogManager.getLogger(this.getClass)
+  // Todo: Look into a cats effect friendly logger
+  private given logger: Logger = LogManager.getLogger(this.getClass)
 
   def handleRequest(input: util.HashMap[String, String], context: Context): String =
     // Todo: See if there's a way to not need to call unsafeRunSync()
     run(input.asScala.toMap, context).unsafeRunSync()
 
-  private def run(input: Map[String, String], context: Context): IO[String] = {
+  private def run(input: Map[String, String], context: Context): IO[String] = 
+    // Todo: Look into best way to handle errors
     for {
       _ <- IO(logger.info(s"input = $input"))
       _ <- IO(logger.info(s"name = ${context.getFunctionName}"))
@@ -36,6 +39,5 @@ class Main extends RequestHandler[util.HashMap[String, String], String] {
       events <- CalendarService.retrieveEvents(credentials, projectName, ownerEmail)
     
       // Todo: Send events data in format expected by email builder lambda
-    } yield events
-  }
+    } yield events.toPrettyString
 }
--- googleCalendarImporter/src/main/scala/grimes/charles/calendar/CalendarService.scala
+++ googleCalendarImporter/src/main/scala/grimes/charles/calendar/CalendarService.scala
@@ -1,44 +1,54 @@
 package grimes.charles.calendar
 
-import cats.effect.IO
-import com.google.api.services.calendar.{Calendar, CalendarScopes}
-import com.google.auth.http.HttpCredentialsAdapter
-import com.google.auth.oauth2.GoogleCredentials
-import grimes.charles.credentials.CredentialsLoader
-import com.google.api.services.calendar.model.Events
+import cats.effect.{Clock, IO}
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
 import com.google.api.client.util.DateTime
 import com.google.api.services.calendar
-import java.util.Date
+import com.google.api.services.calendar.Calendar
+import com.google.api.services.calendar.model.Events
+import com.google.auth.http.HttpCredentialsAdapter
+import com.google.auth.oauth2.GoogleCredentials
+import org.apache.logging.log4j.Logger
+
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.time.temporal.ChronoUnit.*
+import java.util.Date
 
 object CalendarService {
-  private def buildCalendarService(credentials: GoogleCredentials, projectName: String): Calendar = 
-    Calendar
-      .Builder(
-        GoogleNetHttpTransport.newTrustedTransport(),
-        GsonFactory.getDefaultInstance,
-        HttpCredentialsAdapter(credentials)
-      )
-      .setApplicationName(projectName)
-      .build()
+  private def buildCalendarService(credentials: GoogleCredentials, projectName: String): IO[Calendar] =
+    IO.blocking(GoogleNetHttpTransport.newTrustedTransport()).map { httpTransport =>
+      Calendar
+        .Builder(
+          httpTransport,
+          GsonFactory.getDefaultInstance,
+          HttpCredentialsAdapter(credentials)
+        )
+        .setApplicationName(projectName)
+        .build()
+    }
 
-  def retrieveEvents(credentials: GoogleCredentials, projectName: String, ownerEmail: String): IO[String] = 
-    // Todo: Handle this better
-    IO {
-      buildCalendarService(credentials, projectName)
+  def retrieveEvents(credentials: GoogleCredentials, projectName: String, ownerEmail: String)
+                    (using clock: Clock[IO], logger: Logger): IO[Events] =
+    for {
+      calendarService <- buildCalendarService(credentials, projectName)
+
+      now <- clock.realTimeInstant
+      timeMin = Date.from(now)
+      timeMax = Date.from(now.plus(7, DAYS))  // Todo: Make this configurable.
+      
+      _ <- IO(logger.info(s"Retrieving events from $timeMin to $timeMax"))
+      
+      eventsRequest = calendarService
         .events()
         .list(ownerEmail)
         .setSingleEvents(true)
         .setOrderBy("starttime")
-        .setTimeMin(DateTime(Date.from(Instant.now())))
-        .setTimeMax(DateTime(Date.from(Instant.now().plus(7, DAYS)))) // Todo: Make this configurable.
+        .setTimeMin(DateTime(timeMin))
+        .setTimeMax(DateTime(timeMax)) 
         .setTimeZone("Europe/London")
         .setMaxResults(10)
-        .execute()
-        .toPrettyString
-    }
+      events <- IO.blocking(eventsRequest.execute())
+    } yield events
 }
